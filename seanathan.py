f3512aadac21ed5abcca04308bd13742f0874ccc
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-29 21:24:11 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -13,9 +13,9 @@ async def unload(ctx,extension):
     client.unload_extension(f'cogs.{extension}')
 
 # Runs at bot startup to load all cogs
-for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'): # go into cog folder and search all files
-    if filename.endswith('.py'): # see if the file is a python file
-        client.load_extension(f'cogs.{filename[:-3]}') # cuts off last 3 chars
+for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'):
+    if filename.endswith('.py'): 
+        client.load_extension(f'cogs.{filename[:-3]}')
 client.run(Token)
 
 
