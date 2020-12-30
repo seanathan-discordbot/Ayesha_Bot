c0ea9ae88c1b03d3f51d421564f2c85edaaa7b74
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-30 16:14:55 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -6,14 +6,16 @@ client=commands.Bot(command_prefix="!")
 
 @client.command()
 async def load(ctx,extension):
+    await ctx.message.channel.send('loaded')
     client.load_extension(f'cogs.{extension}')
 
 @client.command()
 async def unload(ctx,extension):
+    await.ctx.message.send('unloaded')
     client.unload_extension(f'cogs.{extension}')
 
 # Runs at bot startup to load all cogs
-for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'):
+for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'): #replace with your path
     if filename.endswith('.py'): 
         client.load_extension(f'cogs.{filename[:-3]}')
 client.run(Token)
