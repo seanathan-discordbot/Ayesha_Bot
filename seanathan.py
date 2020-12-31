e75d9f96bc99ab69a1910d8e577702c6dc27dde7
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-31 14:01:10 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -4,15 +4,23 @@ from discord.ext import commands
 Token
 client=commands.Bot(command_prefix="!")
 admins = [196465885148479489, 325080171591761921, 530760994289483790, 145339105239105537] #Aramy, Sean, Demi, Roberto
+
 def is_admin(ctx):
         if ctx.author.id in admins:
             return True
+
 @client.command()
 @commands.check(is_admin)
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.channel.send('Loaded.')
 
+@client.command()
+@commands.check(is_admin)
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.channel.send('Unloaded.')
+ 
 @client.command()
 @commands.check(is_admin)
 async def reload(ctx, extension):
