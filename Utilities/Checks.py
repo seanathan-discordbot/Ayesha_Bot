6e425023ec4ae04b7ae615a9e65a43f1b810199e
Aramythia <54689554+Aramythia@users.noreply.github.com>
2021-01-08 16:34:32 -0500

    Add Checks

---
+++ Utilities/Checks.py
@@ -0,0 +1,24 @@
+import discord
+import asyncio
+
+from discord.ext import commands, menus
+
+import aiosqlite
+
+PATH = r'C:\Users\sebas\OneDrive\NguyenBot\Database\AlphaDB.db'
+
+async def not_player(ctx):
+    query = (ctx.author.id,)
+    async with aiosqlite.connect(PATH) as conn:
+        c = await conn.execute('SELECT * FROM players WHERE user_id = ?', query)
+        result = await c.fetchone()
+        if result is None: #Then there is no char for this id
+            return True
+
+async def is_player(ctx):
+    query = (ctx.author.id,)
+    async with aiosqlite.connect(PATH) as conn:
+        c = await conn.execute('SELECT * FROM players WHERE user_id = ?', query)
+        result = await c.fetchone()
+        if result is not None: #Then there is a char for this id
+            return True
