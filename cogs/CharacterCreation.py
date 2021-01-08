fea0732a7f7d0b7094130adb1c26b1b2c3feedbd
Aramythia <54689554+Aramythia@users.noreply.github.com>
2021-01-08 16:33:42 -0500

    CC, Classes

---
+++ cogs/CharacterCreation.py
@@ -0,0 +1,143 @@
+import discord
+import asyncio
+
+from discord.ext import commands, menus
+from discord.ext.commands import BucketType, cooldown, CommandOnCooldown
+
+import aiosqlite
+from Utilities import Checks
+
+PATH = r'C:\Users\sebas\OneDrive\NguyenBot\Database\AlphaDB.db'
+
+class YesNo(menus.Menu):
+    def __init__(self, ctx, embed):
+        super().__init__(timeout=15.0, delete_message_after=True)
+        self.embed = embed
+        self.result = None
+        
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(embed=self.embed)
+    
+    async def prompt(self, ctx):
+        await self.start(ctx, wait=True)
+        return self.result
+    
+    @menus.button('\u2705') # Check mark
+    async def on_yes(self, payload):
+        self.result = True
+        self.stop()
+        
+    @menus.button('\u274E') # X
+    async def on_no(self, payload):
+        self.result = False
+        self.stop() 
+
+class CharacterCreation(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    #EVENTS
+    @commands.Cog.listener() # needed to create event in cog
+    async def on_ready(self): # YOU NEED SELF IN COGS
+        print('CC is ready.')
+
+    #INVISIBLE
+    async def createCharacter(self, ctx, name):
+        user = (ctx.author.id, name)
+        async with aiosqlite.connect(PATH) as conn:
+            await conn.execute("""
+                INSERT INTO players (user_id, user_name)
+                VALUES (?, ?)""",
+                user)
+            await conn.commit()
+        await self.createItem('Spear', ctx.author.id, 20, 'Wooden Spear', 'Common')
+
+
+    async def createItem(self, weapontype, owner_id, attack, name, rarity):
+        async with aiosqlite.connect(PATH) as conn:
+            item = (weapontype, owner_id, attack, name, rarity)
+            await conn.execute("""
+                INSERT INTO items (weapontype, owner_id, attack, weapon_name, rarity)
+                VALUES (?, ?, ?, ?, ?)""",
+                item)
+            await conn.commit()
+
+    #COMMANDS
+    @commands.command(aliases=['begin','create'], brief='<name : str>', description='Start the game of Seanathan')
+    @commands.check(Checks.not_player)
+    async def start(self, ctx, *, name : str = None):
+        if not name:
+            name = ctx.author.display_name
+        if len(name) > 32:
+                await ctx.send("Name can only be up to 32 characters")
+        else:
+            prefix = await self.client.get_prefix(ctx.message)
+            embed = discord.Embed(title='Start the game of Seanathan?')
+            embed.add_field(name=f'Your Name: {name}', value=f'You can customize your name by doing `{prefix}start <name>`')
+            start = await YesNo(ctx, embed).prompt(ctx)
+            if start:
+                await ctx.send(f'Your Name: {name}')
+                await self.createCharacter(ctx, name)
+                await ctx.send("Success")  
+
+    @commands.command(aliases=['i', 'inv'], description='View your inventory of items')
+    @commands.check(Checks.is_player)
+    async def inventory(self, ctx):
+        user = (ctx.author.id,)
+        async with aiosqlite.connect(PATH) as conn:
+            c = await conn.execute("""
+                SELECT * FROM Items
+                WHERE owner_id = ?""",
+                user)
+            inv = await c.fetchall()
+        for row in inv:
+            await ctx.send(row)
+
+    @commands.command(brief='<item_id : int>', description='Equip an item from your inventory using its ID')
+    @commands.check(Checks.is_player)
+    async def equip(self, ctx, item_id : int):
+        query = (item_id, ctx.author.id) # Make sure that 1. item exists 2. they own this item
+        async with aiosqlite.connect(PATH) as conn:
+            c = await conn.execute("""
+                SELECT * FROM items
+                WHERE item_id = ? AND owner_id = ?""", 
+                query)
+            item = await c.fetchone()
+            if item is None:
+                await ctx.send("No.")
+            else: #Equip new item, update new item, update old item
+                user = (ctx.author.id,)
+                c = await conn.execute("""
+                SELECT item_id, owner_id, is_equipped
+                FROM Items
+                WHERE owner_id = ?
+                AND is_equipped = 1""", user)
+                olditem = await c.fetchone()
+                if olditem is not None: #Skip updating old item if there is no old item
+                    if olditem[0] == item_id: #If they try to re-equip their item
+                        await ctx.send("This item is already equipped")
+                        return
+                    else:
+                        await conn.execute("""
+                        UPDATE Items
+                        SET is_equipped = 0
+                        WHERE item_id = ?""", (olditem[0],))
+                await conn.execute("""
+                UPDATE Items
+                SET is_equipped = 1
+                WHERE item_id = ?""", (query[0],))
+                await conn.execute("""
+                UPDATE Players
+                SET equipped_item = ?
+                WHERE user_id = ?""", query)
+                if olditem is not None:
+                    await ctx.send(f'Unequipped item {olditem[0]} and equipped {item_id}')
+                else:
+                    await ctx.send(f'Equipped item {item_id}')
+            await conn.commit()
+        
+
+
+def setup(client):
+    client.add_cog(CharacterCreation(client))
---
+++ cogs/Classes.py
@@ -0,0 +1,166 @@
+import discord
+import asyncio
+
+from discord.ext import commands, menus
+from discord.ext.commands import BucketType, cooldown, CommandOnCooldown
+
+import aiosqlite
+
+PATH = r'C:\Users\sebas\OneDrive\NguyenBot\Database\AlphaDB.db'
+
+occupations = {
+    'Soldier' : 'You are a retainer of a local lord, trained in the discipline of swordsmanship.\nYou have an increased character attack',
+    'Blacksmith' : 'You\'ve spent years as the apprentice of a hardy blacksmith, and now a master in the art of forging.\n You gain a bonus in weapon attack',
+    'Farmer' : 'You are a lowly farmer, but farming is no easy job.\nYou gain increased resources from labor commands.',
+    'Hunter' : 'The wild is your domain; no game unconquerable.\nYou gain increased rewards from hunting.',
+    'Merchant' : 'Screw you, exploiter of others\' labor.\nYou gain increased income from selling items.',
+    'Traveler' : 'The wild forests up north await, as do the raging seas to the south. What will you discover?\nYou gain increased rewards from travel.',
+    'Leatherworker' : 'The finest protective gear, saddles, and equipment have your name on it.\nYou have increased HP',
+    'Butcher' : 'Meat. What would one do without it?\nYou have increased healing effectiveness.',
+    'Engineer' : 'Your lord praises the seemingly impossible design of his new manor.\nYou will gain increased compensation from your association',
+    'Scribe' : 'Despite the might of your lord, you\'ve learned a little bit about everything too.\nYou have an increased crit rate'
+}
+
+origins = {
+    'Aramithea' : 'You\'re a metropolitan. Aramithea, the largest city on Rabidus, must have at least a million people, and a niche for everybody',
+    'Riverburn' : 'The great rival of Aramithea; Will you bring your town to victory?',
+    'Thenuille' : 'You love the sea; you love exploration; you love trade. From here one can go anywhere, and be anything',
+    'Mythic Forest' : 'You come from the lands down south, covered in forest. You could probably hit a deer square between the eyes blindfolded.',
+    'Sunset' : 'Nothing is more peaceful than an autumn afternoon in the prairie.',
+    'Lunaris' : 'The crossroads of civilization; the battleground of those from the north, west, and east. Your times here have hardened you.',
+    'Crumidia' : 'The foothills have turned you into a strong warrior. Perhaps you will seek domination over your adversaries?',
+    'Maritimiala' : 'North of the mountains, the Maritimialan tribes look lustfully upon the fertile plains below. Will you seek integration, or domination?',
+    'Glakelys' : 'The small towns beyond Riverburn disregard the Aramithean elite. The first line of defense from invasions from Lunaris, the Glakelys are as tribal as they were 300 years ago.'
+}
+
+occ = enumerate(occupations)
+occ = list(occ)
+
+# 0. Soldier - 15% bonus to character attack
+# 1. Blacksmith - 10% bonus to weapon attack
+# 2. Farmer - increased resources from labor commands
+# 3. Hunter - increased travelling/(hunting?) rewards
+# 4. Merchant - increased income from selling items
+# 5. Traveler - increased travelling rewards
+# 6. Leatherworker/Tailor - increased HP (or DEF if added)
+# 7. Butcher - increased healing effectiveness
+# 8. Engineer/Mechanic - buff association bonuses slightly
+# 9. Scribe - increased crit rate
+
+ori = enumerate(origins)
+ori = list(ori)
+
+class Classes(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    #EVENTS
+    @commands.Cog.listener() # needed to create event in cog
+    async def on_ready(self): # YOU NEED SELF IN COGS
+        print('Classes is ready.')
+
+    #COMMANDS
+    @cooldown(1, 3600, BucketType.user)
+    @commands.command(aliases=['class'], description='Choose your player class. This can be changed.')
+    async def changeclass(self, ctx):
+        entries = []
+        for job in occupations:
+            embed = discord.Embed(title='Class Selection Menu', color=0xBEDCF6)
+            embed.add_field(name=f'{job}: Choose \u2705 to take this class!', value=f'{occupations[job]}')
+            entries.append(embed)
+        message = await ctx.send(embed=entries[0])
+        await message.add_reaction('\u23EE') #Left
+        await message.add_reaction('\u2705') #Check
+        await message.add_reaction('\u274E') #X
+        await message.add_reaction('\u23ED') #Right
+
+        def check(reaction, user):
+            return user == ctx.author
+
+        page = 0
+        reaction = None
+        readReactions = True
+
+        while readReactions:
+            if str(reaction) == '\u23EE':
+                if page > 0:
+                    page -= 1
+                    await message.edit(embed=entries[page])
+            if str(reaction) == '\u23ED':
+                if page < 9:
+                    page += 1
+                    await message.edit(embed=entries[page])
+            if str(reaction) == '\u274E':
+                await message.delete()
+                await ctx.send('No class chosen.')
+                break
+            if str(reaction) == '\u2705': # Then change class
+                role = occ[page][1]
+                query = (role, ctx.author.id)
+                async with aiosqlite.connect(PATH) as conn:
+                    await conn.execute("UPDATE Players SET class = $1 WHERE user_id = $2;", query)
+                    await conn.commit()
+                await ctx.send(f'{ctx.author.mention}, you are now a {role}!')
+                await message.delete()
+                break
+
+            try:
+                reaction, user = await self.client.wait_for('reaction_add', check=check, timeout=60.0)
+                await message.remove_reaction(reaction, user)
+            except asyncio.TimeoutError:
+                readReactions = not readReactions
+                await message.delete()
+        
+    @commands.command(aliases=['background','birthplace'], description='Choose your birthplace.')
+    async def origin(self, ctx):
+        entries = []
+        for place in origins:
+            embed = discord.Embed(title='Background Selection Menu', color=0xBEDCF6)
+            embed.add_field(name=f'{place}: Choose \u2705 if you like this place!', value=f'{origins[place]}')
+            entries.append(embed)
+        message = await ctx.send(embed=entries[0])
+        await message.add_reaction('\u23EE') #Left
+        await message.add_reaction('\u2705') #Check
+        await message.add_reaction('\u274E') #X
+        await message.add_reaction('\u23ED') #Right
+
+        def check(reaction, user):
+            return user == ctx.author
+
+        page = 0
+        reaction = None
+        readReactions = True
+
+        while readReactions:
+            if str(reaction) == '\u23EE':
+                if page > 0:
+                    page -= 1
+                    await message.edit(embed=entries[page])
+            if str(reaction) == '\u23ED':
+                if page < 8:
+                    page += 1
+                    await message.edit(embed=entries[page])
+            if str(reaction) == '\u274E':
+                await message.delete()
+                await ctx.send('Nothing chosen.')
+                break
+            if str(reaction) == '\u2705': # Then change class
+                place = ori[page][1]
+                query = (place, ctx.author.id)
+                async with aiosqlite.connect(PATH) as conn:
+                    await conn.execute("UPDATE Players SET origin = $1 WHERE user_id = $2;", query)
+                    await conn.commit()
+                await ctx.send(f'{ctx.author.mention}, you are from {place}!')
+                await message.delete()
+                break
+
+            try:
+                reaction, user = await self.client.wait_for('reaction_add', check=check, timeout=60.0)
+                await message.remove_reaction(reaction, user)
+            except asyncio.TimeoutError:
+                readReactions = not readReactions
+                await message.delete()
+
+def setup(client):
+    client.add_cog(Classes(client))
