c1e41413ba356f4b04f48543cf4a8a2cdd1011ac
Aramythia <54689554+Aramythia@users.noreply.github.com>
2021-01-21 17:31:23 -0500

    Add files via upload

--- Utilities/PageSourceMaker.py
+++ Utilities/PageSourceMaker.py
@@ -7,3 +7,26 @@ class PageMaker(menus.ListPageSource):
 
     async def format_page(self, menu, entries): #Guys I still don't understand this module
         return entries
+
+class YesNo(menus.Menu):
+    def __init__(self, ctx, embed):
+        super().__init__(timeout=15.0, delete_message_after=True)
+        self.embed = embed
+        self.result = None
+        
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(embed=self.embed)
+    
+    async def prompt(self, ctx):
+        await self.start(ctx, wait=True)
+        return self.result
+    
+    @menus.button('\u2705') # Check mark
+    async def on_yes(self, payload):
+        self.result = True
+        self.stop()
+        
+    @menus.button('\u274E') # X
+    async def on_no(self, payload):
+        self.result = False
+        self.stop() 
