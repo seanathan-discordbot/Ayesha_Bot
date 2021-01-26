0c1e76e9c44ef183a4349b24bef813f1d127f7c0
Aramythia <54689554+Aramythia@users.noreply.github.com>
2021-01-26 18:35:03 -0500

    Buff Iron and Silver From Mine, Cacao from Forage

---
+++ Acolytes.py
@@ -0,0 +1,162 @@
+import discord
+import asyncio
+
+from discord.ext import commands, menus
+from discord.ext.commands import BucketType, cooldown, CommandOnCooldown
+
+from Utilities import Checks, AssetCreation, PageSourceMaker
+
+import random
+import math
+
+class Acolytes(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    #EVENTS
+    @commands.Cog.listener() # needed to create event in cog
+    async def on_ready(self): # YOU NEED SELF IN COGS
+        print('Acolytes is ready.')
+
+    #INVISIBLE
+    def write(self, start, inv, player):
+        embed = discord.Embed(title=f'{player}\'s Tavern', color=0xBEDCF6)
+
+        iteration = 0
+        while start < len(inv) and iteration < 5: #Loop til 5 entries or none left
+            if inv[start][3] == 1 or inv[start][3] == 2:
+                acolyte = AssetCreation.getAcolyteByName(inv[start][1])
+                embed.add_field(name = f"({acolyte['Rarity']}\u2B50) {inv[start][1]}: `{inv[start][0]}` [Equipped]",
+                    value = f"**Level:** {inv[start][2]}, **Attack:** {int(acolyte['Attack'] + (acolyte['Scale'] * inv[start][2]))}, **Crit:** {acolyte['Crit']}%, **Dupes:** {inv[start][4]}\n**Effect:** {acolyte['Effect']}",
+                    inline=False
+                )
+            else:
+                acolyte = AssetCreation.getAcolyteByName(inv[start][1])
+                embed.add_field(name = f"({acolyte['Rarity']}\u2B50) {inv[start][1]}: `{inv[start][0]}`",
+                    value = f"**Level:** {inv[start][2]}, **Attack:** {int(acolyte['Attack'] + (acolyte['Scale'] * inv[start][2]))}, **Crit:** {acolyte['Crit']}%, **Dupes:** {inv[start][4]}\n**Effect:** {acolyte['Effect']}",
+                    inline=False
+                )
+            iteration += 1
+            start += 1
+        return embed
+    
+    #COMMANDS
+    @commands.command(aliases=['acolytes'], description='View your acolytes')
+    @commands.check(Checks.is_player)
+    async def tavern(self, ctx):
+        inv = await AssetCreation.getAllAcolytesFromPlayer(self.client.pg_con, ctx.author.id)
+
+        invpages = []
+        for i in range(0, len(inv), 5): #list 5 entries at a time
+            invpages.append(self.write(i, inv, ctx.author.display_name)) # Write will create the embeds
+        if len(invpages) == 0:
+            await ctx.reply('Your tavern is empty!')
+        else:
+            tavern = menus.MenuPages(source=PageSourceMaker.PageMaker(invpages), clear_reactions_after=True, delete_message_after=True)
+            await tavern.start(ctx)
+
+    @commands.command(brief='<acolyte_id : int> <slot : int>', description='Add an acolyte to your party')
+    @commands.check(Checks.is_player)
+    async def recruit(self, ctx, instance_id : int, slot : int):
+        if slot < 1 or slot > 2:
+            await ctx.reply('You can only place an acolyte in slots 1 or 2 of your party.')
+            return
+
+        if not await AssetCreation.verifyAcolyteOwnership(self.client.pg_con, instance_id, ctx.author.id):
+            await ctx.reply('This acolyte isn\'t in your tavern.')
+            return
+
+        #Equip new acolyte, update new acolyte, update old acolyte
+        acolyte1, acolyte2 = await AssetCreation.getAcolyteFromPlayer(self.client.pg_con, ctx.author.id)
+        
+        if slot == 1:
+            oldacolyte = acolyte1
+            other = acolyte2
+        else:
+            oldacolyte = acolyte2
+            other = acolyte1
+
+        #Make sure they don't try to use the same acolyte twice - if you find a better way to do this change it
+        try:
+            if oldacolyte == instance_id:
+                await ctx.send('This acolyte is already in your party.')
+                return
+        except TypeError:
+            pass
+
+        try:
+            if other == instance_id:
+                await ctx.send('This acolyte is already in your party.')
+                return
+        except TypeError:
+            pass
+
+        try:
+            await AssetCreation.unequipAcolyte(self.client.pg_con, oldacolyte)
+        except TypeError:
+            pass
+
+        # Otherwise add the new acolyte
+        added = await AssetCreation.getAcolyteByID(self.client.pg_con, instance_id)
+        await AssetCreation.equipAcolyte(self.client.pg_con, instance_id, slot, ctx.author.id)
+        await ctx.reply(f"Added `{instance_id}: {added['Name']}` to your party.")
+
+    @commands.command(brief='<slot : int>', description='Dismiss an acolyte from the slot in your party.')
+    @commands.check(Checks.is_player)
+    async def dismiss(self, ctx, slot : int):
+        if slot < 1 or slot > 2:
+            await ctx.reply('Please input a valid slot (1 or 2).')
+            return
+        
+        #Make sure the slot they ask for has something in it
+        acolyte1, acolyte2 = await AssetCreation.getAcolyteFromPlayer(self.client.pg_con, ctx.author.id)
+        if slot == 1:
+            if acolyte1 is None:
+                await ctx.reply('You don\'t have an acolyte equipped in that slot.')
+                return
+        else:
+            if acolyte2 is None:
+                await ctx.reply('You don\'t have an acolyte equipped in that slot.')
+                return
+
+        #Unequip the acolyte
+        if slot == 1:
+            await AssetCreation.unequipAcolyte(acolyte1, 1, ctx.author.id)
+            removed = await AssetCreation.getAcolyteByID(self.client.pg_con, acolyte1)
+        if slot == 2:
+            await AssetCreation.unequipAcolyte(acolyte2, 2, ctx.author.id)
+            removed = await AssetCreation.getAcolyteByID(self.client.pg_con, acolyte2)
+
+        await ctx.reply(f"Dismissed acolyte `{removed['ID']}: {removed['Name']}`")
+
+    @commands.command(brief='<acolyte id>', description='Train your acolyte, giving it xp and potentially levelling it up!')
+    @commands.check(Checks.is_player)
+    async def train(self, ctx, instance_id : int):
+        if not await AssetCreation.verifyAcolyteOwnership(self.client.pg_con, instance_id, ctx.author.id):
+            await ctx.reply('This acolyte isn\'t in your tavern.')
+            return
+
+        acolyte = await AssetCreation.getAcolyteByID(self.client.pg_con, instance_id)
+        if acolyte['Level'] >= 100:
+            await ctx.reply(f"{acolyte['Name']} is already at maximum level!")
+            return
+
+        #Make sure player has the resources and gold to train
+        #5000 xp = 50 of the mat + 250 gold
+        material = await AssetCreation.getPlayerMat(self.client.pg_con, acolyte['Mat'], ctx.author.id)
+        gold = await AssetCreation.getGold(self.client.pg_con, ctx.author.id)
+
+        if material < 50 or gold < 250:
+            await ctx.reply(f"Training your acolyte costs `50` {acolyte['Mat']} and `250` gold. You don\'t have enough resources to train.")
+            return
+        
+        await AssetCreation.giveAcolyteXP(self.client.pg_con, 5000, instance_id)
+        await AssetCreation.giveGold(self.client.pg_con, -250, ctx.author.id)
+        await AssetCreation.takeMat(self.client.pg_con, acolyte['Mat'], 50, ctx.author.id)
+
+        await ctx.reply(f"You trained with `{acolyte['Name']}`, consuming `50` {acolyte['Mat']} and `250` gold in the process. As a result, `{acolyte['Name']}` gained 5,000 exp!")
+        await AssetCreation.checkAcolyteLevel(self.client.pg_con, ctx, instance_id)
+
+def setup(client):
+    client.add_cog(Acolytes(client))
---
+++ Gacha.py
@@ -0,0 +1,144 @@
+import discord
+
+import json
+
+from Utilities import Checks, AssetCreation, Links
+from discord.ext import commands
+
+import asyncpg
+
+import random
+import ayesha
+
+#Randomize at bot startup
+l=list(range(1,101))
+random.shuffle(l)
+two_star=l[0:60]
+three_star=l[60:95]
+four_star=l[95:98]
+one_star=[l[98]]
+five_star=[l[99]]
+
+class Gacha(commands.Cog):
+    def __init__(self,client):
+        self.client=client
+
+    #EVENTS
+    @commands.Cog.listener() # needed to create event in cog
+    async def on_ready(self): # YOU NEED SELF IN COGS
+        print('Gacha is ready.')
+
+    #COMMANDS
+    # @commands.command()
+    # @commands.check(ayesha.is_admin)
+    # async def add(self, ctx, returnStatement):
+    #     acolyte=returnStatement
+    #     await AssetCreation.createAcolyte(self.client.pg_con, ctx.author.id, acolyte)
+
+    @commands.command(aliases=['summon'], description='Spend 1 rubidic to get a random acolyte or weapon!')
+    @commands.check(Checks.is_player)
+    async def roll(self,ctx):
+        #Make sure they have rubidics
+        info = await AssetCreation.getRubidics(self.client.pg_con, ctx.author.id)
+        if info['rubidic'] < 1:
+            await ctx.reply(f"You don\'t have any rubidics to summon. Do `daily` or level up to get some!")
+            return
+        #Load the list of acolytes
+        with open(Links.acolyte_list) as f:
+            acolyte_list=json.load(f) #This stores all the acolytes' info
+            star_data=dict() #This sorts all the acolytes by their rarity
+            for key in acolyte_list:
+                if acolyte_list[key]["Rarity"] not in star_data:
+                    star_data[acolyte_list[key]['Rarity']]=[key]
+                else:
+                    star_data[acolyte_list[key]['Rarity']].append(key)
+
+        #Check to see if player has reached pity
+        if info['pitycounter'] >= 80:
+            name=random.choice(star_data[5])
+            pity = 0
+
+        else: 
+            winner=random.randint(1,101)
+
+            #Determine if reward will be a weapon
+            reward = random.choices(['weapon', 'acolyte'], [75, 25])
+
+            #Select a random weapon or acolyte
+            if winner in five_star: #If its a five star reset the pity
+                if reward[0] == 'weapon':
+                    item_info = await AssetCreation.createItem(self.client.pg_con, ctx.author.id, random.randint(100, 150), 'Legendary', returnstats=True)
+                else:
+                    name=random.choice(star_data[5])
+                pity = 0
+            else: #Add one to the pitycounter
+                if winner in two_star:
+                    if reward[0] == 'weapon':
+                        item_info = await AssetCreation.createItem(self.client.pg_con, ctx.author.id, random.randint(30,60), 'Uncommon', returnstats=True)
+                    else:
+                        name=random.choice(star_data[2])
+
+                elif winner in three_star:
+                    if reward[0] == 'weapon':
+                        item_info = await AssetCreation.createItem(self.client.pg_con, ctx.author.id, random.randint(45,90), 'Rare', returnstats=True)
+                    else:
+                        name=random.choice(star_data[3])
+
+                elif winner in four_star:
+                    if reward[0] == 'weapon':
+                        item_info = await AssetCreation.createItem(self.client.pg_con, ctx.author.id, random.randint(75,120), 'Epic', returnstats=True)
+                    else:
+                        name=random.choice(star_data[4])
+
+                elif winner in one_star:
+                    if reward[0] == 'weapon': #Minimum 2 star weapons from gacha
+                        item_info = await AssetCreation.createItem(self.client.pg_con, ctx.author.id, random.randint(30,60), 'Uncommon', returnstats=True)
+                    else:
+                        name=random.choice(star_data[1])
+
+                pity = info['pitycounter'] + 1
+
+        #With acolyte chosen, update rubidics and pity counter
+        await AssetCreation.setPityCounter(self.client.pg_con, ctx.author.id, pity)
+        rubidics = info['rubidic'] - 1
+        await AssetCreation.setRubidics(self.client.pg_con, ctx.author.id, rubidics)
+
+        #Add acolyte to inventory or add as a duplicate
+        if reward[0] == 'acolyte':
+            is_duplicate = await AssetCreation.checkDuplicate(self.client.pg_con, ctx.author.id, name)
+
+            if is_duplicate is not None: #then its a dupe - add 1 to duplicate
+                await AssetCreation.addAcolyteDuplicate(self.client.pg_con, is_duplicate['instance_id'])
+            else: #Create a new one and add it to tavern
+                await AssetCreation.createAcolyte(self.client.pg_con, ctx.author.id, name)
+
+        #Send embed
+        if reward[0] == 'acolyte':
+            embed = discord.Embed(title=f"{name} ({acolyte_list[name]['Rarity']}⭐) has entered the tavern!", color=0xBEDCF6)
+            if acolyte_list[name]['Image'] is not None:
+                embed.set_thumbnail(url=f"{acolyte_list[name]['Image']}")
+            embed.add_field(name='Attack', value=f"{acolyte_list[name]['Attack']} + {acolyte_list[name]['Scale']}/lvl")
+            embed.add_field(name='Crit', value=f"{acolyte_list[name]['Crit']}")
+            embed.add_field(name='HP', value=f"{acolyte_list[name]['HP']}")
+            if acolyte_list[name]['Effect'] is None:
+                embed.add_field(name='Effect', value=f" No effect.\n{name} uses {acolyte_list[name]['Mat']} to level up.")
+            else:
+                embed.add_field(name='Effect', value=f"{acolyte_list[name]['Effect']}\n{name} uses `{acolyte_list[name]['Mat']}` to level up.", inline=False)
+            embed.set_footer(text=f"You have {rubidics} rubidics. You will receive a 5-star in {80-pity} summons.")
+        else:
+            embed = discord.Embed(title=f"You received {item_info['Name']} ({item_info['Rarity']})!", color=0xBEDCF6)
+            embed.add_field(name='Type', value=f"{item_info['Type']}")
+            embed.add_field(name='Attack', value=f"{item_info['Attack']}")
+            embed.add_field(name='Crit', value=f"{item_info['Crit']}")
+            embed.set_footer(text=f"You have {rubidics} rubidics. You will receive a 5-star in {80-pity} summons.")
+
+        await ctx.reply(embed=embed)
+
+    @commands.command(aliases=['rolls', 'summons'], description='See how many rubidics you have and how many more summons are needed until receiving guaranteed 5*')
+    @commands.check(Checks.is_player)
+    async def rubidics(self, ctx):
+        info = await AssetCreation.getRubidics(self.client.pg_con, ctx.author.id)
+        await ctx.reply(f"You have **{info['rubidic']}** rubidics.\nYou will get a 5⭐ weapon or acolyte in **{80-info['pitycounter']}** summons.")
+
+def setup(client):
+    client.add_cog(Gacha(client))
